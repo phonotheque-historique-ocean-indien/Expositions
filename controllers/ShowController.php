217a7dab8248929f383de50dea970effa9dd3747
Gautier Michelin <gm@ideesculture.com>
2021-06-17 05:53:00 +0400

    publish, unpublish, delete functions

--- controllers/ShowController.php
+++ controllers/ShowController.php
@@ -31,10 +31,11 @@ class ShowController extends ActionController
 
     public function __construct(&$po_request, &$po_response, $pa_view_paths = null)
     {
+        session_start();
+        $_SESSION["partie"] = "chaude";
         parent::__construct($po_request, $po_response, $pa_view_paths);
 
         $this->opo_config = Configuration::load(__CA_APP_DIR__ . '/plugins/Expositions/conf/expositions.conf');
-
     }
 
     # -------------------------------------------------------
@@ -42,9 +43,20 @@ class ShowController extends ActionController
     # -------------------------------------------------------
     public function Index($type = "")
     {
+        session_start();
+        $_SESSION["partie"] = "chaude";
+
         $all_articles = ca_site_pages::getPageList();
         $all_articles = array_reverse($all_articles);
         $articles = [];
+        $vt_user = $this->request->getUser();
+        $roles = $vt_user->getUserGroups();
+        $is_redactor = false;
+        foreach($roles as $role) {
+            if($role["code"]=="redactor") {
+                $is_redactor = true;}
+        }
+
         foreach ($all_articles as $testarticle) {
             if ($testarticle["template_title"]=="exposition") {
                 $articles[] = $testarticle;
@@ -55,11 +67,18 @@ class ShowController extends ActionController
         foreach ($articles as $art) {
             $page = new ca_site_pages($art["page_id"]);
             $article = $page->get("content");
+            if(!$page->get("access") && !$is_redactor) continue;
+
             $this->view->setVar("article", $article);
+            $this->view->setVar("access", $page->get("access"));
             $this->view->setVar("id", $art["page_id"]);
+            $this->view->setVar("is_redactor", $is_redactor);
+
             $blocks .= $this->render("home_block_html.php", true);
         }
         //$page = new ca_site_pages(1);
+        $this->view->setVar("is_redactor", $is_redactor);
+
         $this->view->setVar("blocks", $blocks);
         $this->render('index_html.php');
     }
@@ -93,16 +112,65 @@ class ShowController extends ActionController
     }
 
     public function Details() {
+        $is_redactor = false;
+        foreach($this->getRequest()->getUser()->getUserGroups() as $group) {
+            if($group["code"] == "redactor") $is_redactor=true;
+        }
         $id= $this->request->getParameter("id", pInteger);
         // TODO Redirect if no ID
         $page = new ca_site_pages($id);
+        //$page = new ca_site_pages(1);
         $article = $page->get("content");
-        $this->view->setVar("article", $article);
 
+        $page = new ca_site_pages($id);
+        $this->view->setVar("access", $page->get("access"));
+
+        $this->view->setVar("article", $article);
+        $this->view->setVar("is_redactor", $is_redactor);
+        $this->view->setVar("id", $id);
         $this->render('exposition_html.php');
     }
 
+    public function Publish() {
+        $is_redactor = false;
+        foreach($this->getRequest()->getUser()->getUserGroups() as $group) {
+            if($group["code"] == "redactor") $is_redactor=true;
+        }
+        $id= $this->request->getParameter("id", pInteger);
+        // TODO Redirect if no ID
+        $page = new ca_site_pages($id);
+        $page->setMode(ACCESS_WRITE);
+        $page->set("access", 1);
+        $page->update();
+
+        $this->redirect("/index.php/Expositions/Show/Details/id/".$id);
+    }
+
+    public function Unpublish() {
+        $is_redactor = false;
+        foreach($this->getRequest()->getUser()->getUserGroups() as $group) {
+            if($group["code"] == "redactor") $is_redactor=true;
+        }
+        $id= $this->request->getParameter("id", pInteger);
+        // TODO Redirect if no ID
+        $page = new ca_site_pages($id);
+        $page->setMode(ACCESS_WRITE);
+        $page->set("access", 0);
+        $page->update();
+
+        $this->redirect("/index.php/Expositions/Show/Details/id/".$id);
+    }
+
     public function List() {
+        $vt_user = $this->request->getUser();
+        $roles = $vt_user->getUserGroups();
+
+        $is_redactor = false;
+        foreach($roles as $role) {
+            if($role["code"]=="redactor") {
+                $is_redactor = true;}
+        }
+
         $all_articles = ca_site_pages::getPageList();
         $all_articles = array_reverse($all_articles);
         $articles = [];
@@ -115,12 +183,43 @@ class ShowController extends ActionController
         }
         $result=[];
         foreach($articles as $key=>$article_info) {
-            $article = new ca_site_pages($article_info["page_id"]);
-            $content = $article->get("ca_site_pages.content");
-            $result[$key] = ["page_id"=>$article_info["page_id"], "title"=>$article_info["title"], "content"=>$content];
+            $page = new ca_site_pages($article_info["page_id"]);
+            if(!$page->get("access") && !$is_redactor) continue;
+
+            $article = $page->get("ca_site_pages.content");
+
+            if($article["date_from"] && !$is_redactor) {
+                $date_from = substr($article["date_from"], 6, 4)."-".substr($article["date_from"], 3, 2)."-".substr($article["date_from"], 0, 2);
+                // Ignore if the article is to be published in the future
+                if(time() < strtotime($date_from)) continue;
+            }
+            if($article["date_to"] && !$is_redactor) {
+                $date_to = substr($article["date_to"], 6, 4)."-".substr($article["date_to"], 3, 2)."-".substr($article["date_to"], 0, 2);
+                // Ignore if the article is to be published in the future
+                if(time() > strtotime($date_to)) continue;
+            }
+            $title = ($article["title"] ? $article["title"] : $article_info["title"])." ".$article["subtitle"].($page->get("access") ? '' : '<span class="tag is-warning" style="margin-top:10px;margin-left:12px;">BROUILLON</span>');
+            $result[$key] = ["page_id"=>$article_info["page_id"], "title"=>$title, "content"=>$article];
         }
         $this->view->setVar("articles", $result);
         $this->render('list_html.php');
     }
+
+    public function Delete() {
+        $is_redactor = false;
+        foreach($this->getRequest()->getUser()->getUserGroups() as $group) {
+            if($group["code"] == "redactor") $is_redactor=true;
+        }
+        if(!$is_redactor) die("This function requires redactor privileges.");
+        $id= $this->request->getParameter("id", pInteger);
+        // TODO Redirect if no ID
+        $page = new ca_site_pages($id);
+        $page->setMode(ACCESS_WRITE);
+        $page->delete();
+        $page->update();
+
+        $this->redirect("/index.php/Expositions/Show/index");
+    }
+
 }
 ?>
